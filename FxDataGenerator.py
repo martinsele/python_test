358f0f9a067f875f52f0b0d03568852019de899b
Martin Selecky <martin.selecky@agentfly.com>
2018-03-29 18:28:35 +0200

    Test data generation

--- FxDataGenerator.py
+++ FxDataGenerator.py
@@ -8,7 +8,7 @@ import  tensorflow.contrib.keras as keras
 
 class FxDataGenerator(object):
 
-    def __init__(self, data, trainDataRatio=0.8, normalizeTool=None, randSeed=101):
+    def __init__(self, data, trainDataRatio=0.8, normalizeTool=None, time_samples=24*14, lookahead_period = 4, randSeed=101):
         """
         Initialize FX data generator
         :param data: input data of 3 dimensions [TimeSteps, currencies, OHLC]
@@ -16,15 +16,25 @@ class FxDataGenerator(object):
         :param normalizeTool: sklearn's tool for data normalization. E.g., sklearn.preprocessing.MinMaxScaler.
         By default, uses None
         Data are not stationary, thus normalize data locally when producing batches
+        :param time_samples: number of time steps in learning input data
+        :param lookahead_period: specifies the look-ahead amount for data labeling
+        :param randSeed numpy random seed
         """
         self.data = data
         self.trainRatio = trainDataRatio
         self.CLOSE_IDX = 3
-
-        np.random.seed(randSeed)
+        self.time_samples = time_samples
+        self.lookahead_period = lookahead_period
 
         samples = data.shape[0]
         self.trainSplitIdx = int(samples*trainDataRatio)
+
+        # shuffle train data
+        np.random.seed(randSeed)
+        self.train_start_indexes = np.random.permutation(self.trainSplitIdx - self.time_samples)
+
+        self.train_data_idx = 0                        # Current index of train data
+        self.test_data_idx = self.trainSplitIdx        # Current index of test data
 
         # Prepare normalization of each currency - data not stationary, thus need to normalize only locally
         self.normalizeTool = []
@@ -33,10 +43,9 @@ class FxDataGenerator(object):
                 self.normalizeTool.append(normalizeTool())
                 # self.normalizeTool.append(normalizeTool().fit(data[:self.trainSplitIdx, i, :]))
 
-    def generate_rand_train_data(self, time_samples=24*14, batch_size=10, pct_change_threshold=0.001, currency_idx=0, class_mode='binary', normalize=False):
+    def generate_rand_train_data(self, batch_size=10, pct_change_threshold=0.001, currency_idx=0, class_mode='binary', normalize=False):
         """
         Generate normalized training data samples with labels of X,y
-        :param time_samples: number of time steps in X data
         :param batch_size: used batch size
         :param pct_change_threshold: threshold to label dataset as 0 - buy / 1 - sell / 2 - noop
         :param currency_idx: currency in question (buy/sell/nothing)
@@ -47,18 +56,24 @@ class FxDataGenerator(object):
         if class_mode 'binary', return one-hot encoded array [batch, num_classes], else vector of labels
         """
         # get random start point within the training data
-        rand_start = np.random.randint(0, self.trainSplitIdx-time_samples, (batch_size, ))
+        if self.train_data_idx + batch_size > len(self.train_start_indexes):
+            self.train_data_idx = 0
+
+        rand_start = self.train_start_indexes[self.train_data_idx:(self.train_data_idx + batch_size)]
+
+        # increment current training index
+        self.train_data_idx += batch_size
 
         # prepare batches of training data
-        X = np.zeros([batch_size, time_samples] + list(self.data.shape[1:]))
+        X = np.zeros([batch_size, self.time_samples] + list(self.data.shape[1:]))
         for i in range(batch_size):
-            X[i, :, :, :] = self.data[rand_start[i]:rand_start[i]+time_samples, :, :]
+            X[i, :, :, :] = self.data[rand_start[i]:rand_start[i]+self.time_samples, :, :]
             if normalize:
                 X = self.normalize_data(X)
 
         # prepare data labels
         y = self.get_data_labels_N_steps(rand_start,
-                                         time_samples,
+                                         self.time_samples,
                                          currency_idx,
                                          pct_change_threshold)
         if class_mode == 'binary':
@@ -66,10 +81,9 @@ class FxDataGenerator(object):
 
         return X, y
 
-    def generate_test_data(self, time_samples=24*14, batch_size=10, pct_change_threshold=0.001, currency_idx=0, class_mode='binary', normalize=False):
+    def generate_test_data(self, batch_size=10, pct_change_threshold=0.001, currency_idx=0, class_mode='binary', normalize=False):
         """
         Generate normalized testing data samples with labels of X,y
-        :param time_samples: number of time steps in X data
         :param batch_size: used batch size
         :param pct_change_threshold: threshold to label dataset as 0 - buy / 1 - sell / 2 - noop
         :param currency_idx: currency in question (buy/sell/nothing)
@@ -79,19 +93,31 @@ class FxDataGenerator(object):
         y - vector or matrix of classes, depending on the parameter 'class_mode';
         if class_mode 'binary', return one-hot encoded array [batch, num_classes], else vector of labels
         """
-        # get random start point within the training data
-        rand_start = np.random.randint(0, self.trainSplitIdx-time_samples, (batch_size, ))
+        # update the batch size if too large
+        new_batch_size = batch_size
+        needed_data_size = self.time_samples + self.lookahead_period + 1
+        if self.test_data_idx + batch_size > self.data.shape[0] - needed_data_size:
+            new_batch_size = self.data.shape[0] - needed_data_size - self.test_data_idx
+
+        # prepare start indexes of all test batches
+        incremental_starts = np.arange(self.test_data_idx, (self.test_data_idx + new_batch_size))
+
+        # increment current testing data index
+        if new_batch_size == 0:
+            self.test_data_idx = self.trainSplitIdx
+        else:
+            self.test_data_idx += new_batch_size
 
         # prepare batches of training data
-        X = np.zeros([batch_size, time_samples] + list(self.data.shape[1:]))
-        for i in range(batch_size):
-            X[i, :, :, :] = self.data[rand_start[i]:rand_start[i]+time_samples, :, :]
+        X = np.zeros([new_batch_size, self.time_samples] + list(self.data.shape[1:]))
+        for i in range(new_batch_size):
+            X[i, :, :, :] = self.data[incremental_starts[i]:incremental_starts[i]+self.time_samples, :, :]
             if normalize:
                 X = self.normalize_data(X)
 
         # prepare data labels
-        y = self.get_data_labels_N_steps(rand_start,
-                                         time_samples,
+        y = self.get_data_labels_N_steps(incremental_starts,
+                                         self.time_samples,
                                          currency_idx,
                                          pct_change_threshold)
         if class_mode == 'binary':
@@ -113,24 +139,24 @@ class FxDataGenerator(object):
         :return: if class_mode 'binary', return one-hot encoded array [batch, num_classes], else vector of labels
         """
         batch_size = len(rand_start)
-        y = np.zeros((batch_size, ))
+        y = np.zeros((batch_size, ), dtype=int)
         for i in range(batch_size):
             # get percent changes near the data sample end and two more
             currency_data = self.data[(rand_start[i] + time_samples - 2) : (rand_start[i] + time_samples + 2), currency_idx, self.CLOSE_IDX]
             cur_pct_change = self.pct_change(currency_data)
 
-            if cur_pct_change[-2] > pct_change_threshold: # and cur_pct_change[-1] > 0:
+            if cur_pct_change[-2] > pct_change_threshold:  # and cur_pct_change[-1] > 0:
                 y[i] = 0
-            elif cur_pct_change[-2] < -pct_change_threshold: # and cur_pct_change[-1] < 0:
+            elif cur_pct_change[-2] < -pct_change_threshold:  # and cur_pct_change[-1] < 0:
                 y[i] = 1
             else:
                 y[i] = 2
 
         return y
 
-    def get_data_labels_N_steps(self, rand_start, time_samples, currency_idx, pct_change_threshold, period=4):
+    def get_data_labels_N_steps(self, rand_start, time_samples, currency_idx, pct_change_threshold):
         """
-        Prepare data labels - if the CLOSE price of given currency in next %period% time-steps raises above pct_change_threshold
+        Prepare data labels - if the CLOSE price of given currency in next %lookahead_period% time-steps raises above pct_change_threshold
         -> buy (0).
         If the situation is the opposite, i.e. close price in next 4 time-step falls by at least pct_change_threshold
         -> sell(1).
@@ -139,19 +165,18 @@ class FxDataGenerator(object):
         :param time_samples: number of time steps in training data
         :param currency_idx:
         :param pct_change_threshold: threshold to label dataset as 0 - buy / 1 - sell / 2 - noop
-        :param period: specifies the look-ahead amount
         :return: if class_mode 'binary', return one-hot encoded array [batch, num_classes], else vector of labels
         """
         batch_size = len(rand_start)
-        y = np.zeros((batch_size, ))
+        y = np.zeros((batch_size, ), dtype=int)
         for i in range(batch_size):
             # get percent changes near the data sample end and two more
-            currency_data = self.data[(rand_start[i] + time_samples) : (rand_start[i] + time_samples + 2*period), currency_idx, self.CLOSE_IDX]
-            cur_pct_change = self.pct_change_N(currency_data, period)
+            currency_data = self.data[(rand_start[i] + time_samples) : (rand_start[i] + time_samples + self.lookahead_period + 1), currency_idx, self.CLOSE_IDX]
+            cur_pct_change = self.pct_change_N(currency_data, self.lookahead_period)
 
-            if cur_pct_change[0] > pct_change_threshold: # and cur_pct_change[-1] > 0:
+            if cur_pct_change[0] > pct_change_threshold:
                 y[i] = 0
-            elif cur_pct_change[0] < -pct_change_threshold: # and cur_pct_change[-1] < 0:
+            elif cur_pct_change[0] < -pct_change_threshold:
                 y[i] = 1
             else:
                 y[i] = 2
--- model_learn.py
+++ model_learn.py
@@ -1,6 +1,7 @@
 from DataReader import DataReader
 from FxDataGenerator import FxDataGenerator
 from sklearn.preprocessing import MinMaxScaler
+from keras.callbacks import ModelCheckpoint, EarlyStopping
 import numpy as np
 
 
@@ -9,7 +10,9 @@ import numpy as np
 # EURUSD ma celkem dobre pomery pct_change, proto obchodovat zde -> currency_idx = 3
 # --> Kontroluje se zmena za periodu (4 hod) ><= 0.001
 
-batch_size = 10
+batch_size = 64
+epochs = 20
+train_data_ratio = 0.8
 time_samples = 24*14
 pct_change_threshold = 0.001
 currency_idx = 3
@@ -19,23 +22,51 @@ rand_seed = 101
 reader = DataReader('./data')
 data, labels = reader.read_fx_data_as_array()
 
-generator = FxDataGenerator(data, normalizeTool=MinMaxScaler, randSeed=rand_seed)
+generator = FxDataGenerator(data,
+                            normalizeTool=MinMaxScaler,
+                            time_samples=time_samples,
+                            trainDataRatio=train_data_ratio,
+                            randSeed=rand_seed)
+
+# create model to fit
+# model =
+# checkpointer = ModelCheckpoint(filepath='./weights.hdf5', verbose=1, save_best_only=True)
+# earlystopper = EarlyStopping(monitor='val_loss', min_delta=0, patience=5, verbose=0, mode='auto')
 
 # ---- CHECK THRESHOLD SETTING
 classes = np.zeros(3,)
-for i in range(400):
-    X, y = generator.generate_rand_train_data(time_samples=time_samples,
-                                    batch_size=batch_size,
-                                    pct_change_threshold=pct_change_threshold,
-                                    currency_idx=currency_idx)
-    classes[0] += sum(y[:, 0] == 1)
-    classes[1] += sum(y[:, 1] == 1)
-    classes[2] += sum(y[:, 2] == 1)
+for e in range(epochs):
+    print('Epoch: ', e, '/', epochs)
+
+    # TRAINING
+    num_iter = int(data.shape[0] * train_data_ratio / batch_size) + 1
+    for i in range(num_iter):
+        X, y = generator.generate_rand_train_data(batch_size=batch_size,
+                                                  pct_change_threshold=pct_change_threshold,
+                                                  currency_idx=currency_idx)
+        # model.train_on_batch(X, y)
+
+     # print training error
+
+    # VALIDATION
+    print('Validation:')
+    num_iter = int(data.shape[0] * (1 - train_data_ratio) / batch_size) + 1
+    for i in range(num_iter):
+        X, y = generator.generate_test_data(batch_size=batch_size,
+                                            pct_change_threshold=pct_change_threshold,
+                                            currency_idx=currency_idx)
+        # y_pred = model.predict_on_batch(X)
+        # estimate accuracy
+
+    # print validation error
+
+
+        # classes[0] += sum(y[:, 0] == 1)
+        # classes[1] += sum(y[:, 1] == 1)
+        # classes[2] += sum(y[:, 2] == 1)
 
 print(classes)
 
 # ---- CHECK OUTPUT
 print(X.shape)
 print(y.shape)
-
-# TODO - koukni, jak jsou pct changes rozvrstveny v ramci datasetu pro jednotlive meny
